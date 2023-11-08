7cc0acf8e43ab7fbe7a94e3620031e9cd648e6ed
nimosarang <111734043+nimosarang@users.noreply.github.com>
2023-11-08 22:19:52 +0800

    기본세팅

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,87 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Dropdown
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><hr class="dropdown-divider" /></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+          </li>
+        </ul>
+        <form class="d-flex" role="search">
+          <input
+            class="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+
+  <div class="container mt-4">
+    <h5>React 개발자의 블로그입니다.</h5>
+    <p>- Vue로 만들었음</p>
+  </div>
+
+  <ListVue
+    :blogContents="blogContents[i]"
+    v-for="(blogContent, i) in blogContents"
+    :key="i"
+  />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ListVue from "./components/List.vue";
+import blog from "./assets/blog.js";
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      blogContents: [...blog],
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    ListVue,
+  },
+};
 </script>
 
 <style>
@@ -20,7 +90,5 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/List.vue
@@ -0,0 +1,17 @@
+<template lang="kor">
+    <div>
+        <h5>{{blogContents.title}}</h5>
+        <p>{{blogContents.content}}</p>
+        <p>{{blogContents.date}}</p>
+        <p>{{blogContents.number}}</p>
+    </div>
+</template>
+<script>
+export default {
+  name: "ListVue",
+  props: {
+    blogContents: Array,
+  },
+};
+</script>
+<style lang="kor"></style>
